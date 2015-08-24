e6ac3c46c3e3ec114cbf3acf1cb5e8831280a136
juanonsoftware <juanonsoftware@github.com>
2015-08-24 17:04:39 +0700

    New services

---
+++ TsqgDownloadService/DhmdchnDownloadService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DownloadMaster.Common;
+
+namespace TsqgDownloadService
+{
+    public class DhmdchnDownloadService : PageFilesDownloadService
+    {
+        protected override string GetFileName(string targetUri, string filePattern)
+        {
+            var match = Regex.Matches(targetUri, filePattern, RegexOptions.Singleline).Cast<Match>().First();
+            return Uri.UnescapeDataString(match.Groups[1].Value);
+        }
+
+        protected override IEnumerable<string> GetFileLinks(string pageContent, string filePattern)
+        {
+            return RegexHelper.GetLinks(pageContent).Where(link => Regex.IsMatch(link, filePattern)).Distinct();
+        }
+    }
+}
