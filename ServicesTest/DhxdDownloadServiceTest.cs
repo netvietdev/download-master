3f1e96615035acdcbc22415d954e5e557bec1947
juanonsoftware <juanonsoftware@github.com>
2015-08-25 21:56:01 +0700

    Missing files

---
+++ ServicesTest/DhxdDownloadServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using DownloadMaster.Common;
+using DownloadMaster.GoogleDrive;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class DhxdDownloadServiceTest
+    {
+        [TestMethod]
+        public void CanDownloadFiles()
+        {
+            var service =
+                new DhxdDownloadService("798851632091-eqf7bgvqf797q5lnm7g47t0rq08prks9@developer.gserviceaccount.com",
+                    @"D:\Wip\Practices\ts2015\Docs\GoogleServiceAccount\tuyensinhquocgia-f36c1d0e7788.p12", "TSQG");
+
+            var options = new DownloadServiceOption()
+            {
+                TargetFolder = @"D:\\Wip\\Practices\\OpenSource\\ts2015\\DHXD-HN",
+                UrlsAndPatterns = new Dictionary<string, string>()
+                {
+                    {"http://tuyensinh.nuce.edu.vn/tin-tuc/thong-bao-diem-chuan-va-danh-sach-trung-tuyen-dai-hoc-he-chinh-quy", @".*\/(.*?.pdf)$|google.com"}
+                }
+            };
+
+            service.Download(options);
+        }
+    }
+}
